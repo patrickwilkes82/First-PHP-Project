3d6ea2acc509188473662bbc9892c13a631d6b14
patrickwilkes82 <patwilkes82@gmail.com>
2019-12-11 14:16:30 -0500

    Initial Push

---
+++ header.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name='description' content='G1H Atlas Like System'>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+
+<header> 
+
+</header>
